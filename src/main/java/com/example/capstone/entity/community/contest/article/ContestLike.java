7570b8ba9910ac4287ae6602eb6cbedcb398a48a
nuexs <nuexswe@gmail.com>
2024-03-24 18:53:49 +0900

    feat: 공모전 좋아요 엔티티

---
+++ src/main/java/com/example/capstone/entity/community/contest/article/ContestLike.java
@@ -0,0 +1,19 @@
+package com.example.capstone.entity.community.contest.article;
+
+import com.example.capstone.entity.user.User;
+import jakarta.persistence.*;
+
+@Entity
+@IdClass(ContestLikeId.class)
+public class ContestLike {
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "contest_id")
+    private ContestArticle contestArticle;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "nickname", referencedColumnName = "nickname")
+    private User user;
+}
+
---
+++ src/main/java/com/example/capstone/entity/community/contest/article/ContestLikeId.java
@@ -0,0 +1,8 @@
+package com.example.capstone.entity.community.contest.article;
+
+import java.io.Serializable;
+
+public class ContestLikeId implements Serializable {
+    private Integer contestArticle;
+    private String user;
+}
