d2db081fbee055b3b43c45249f84acdcecb6b84b
nuexs <nuexswe@gmail.com>
2024-03-24 18:52:31 +0900

    feat: 일반 게시판 좋아요, 스크랩 엔티티

---
+++ src/main/java/com/example/capstone/entity/community/general/article/GeneralLike.java
@@ -0,0 +1,19 @@
+package com.example.capstone.entity.community.general.article;
+
+import com.example.capstone.entity.user.User;
+import jakarta.persistence.*;
+
+@Entity
+@IdClass(GeneralLikeId.class)
+public class GeneralLike {
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "nickname", referencedColumnName = "nickname")
+    private User user;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private GeneralArticle generalArticle;
+}
+
---
+++ src/main/java/com/example/capstone/entity/community/general/article/GeneralLikeId.java
@@ -0,0 +1,8 @@
+package com.example.capstone.entity.community.general.article;
+
+import java.io.Serializable;
+
+public class GeneralLikeId implements Serializable {
+    private String user;
+    private Integer generalArticle;
+}
