6b32a58617378c11063b6b43ca6980aaf093e307
nuexs <nuexswe@gmail.com>
2024-03-24 18:54:04 +0900

    feat: 공모전 댓글 엔티티

---
+++ src/main/java/com/example/capstone/entity/community/contest/comment/ContestComment.java
@@ -0,0 +1,35 @@
+package com.example.capstone.entity.community.contest.comment;
+
+import com.example.capstone.entity.community.contest.article.ContestArticle;
+import com.example.capstone.entity.user.User;
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+public class ContestComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer commentId;
+
+    @Column(nullable = false)
+    private Date date;
+
+    private String content;
+
+    private Integer depth;
+
+    @Column(name = "`order`")
+    private Integer order;
+
+    @Column(name = "`group`")
+    private Integer group;
+
+    @ManyToOne
+    @JoinColumn(name = "contest_id")
+    private ContestArticle contestArticle;
+
+    @ManyToOne
+    @JoinColumn(name = "nickname", referencedColumnName = "nickname")
+    private User user;
+}
+
