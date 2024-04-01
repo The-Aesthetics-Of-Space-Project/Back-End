ae705628148844aea7c39866a819ff69f3845c5d
nuexs <nuexswe@gmail.com>
2024-04-01 21:05:23 +0900

    feat: dto 수정

--- src/main/java/com/example/capstone/dto/request/GeneralPostUpdateRequestDto.java
+++ src/main/java/com/example/capstone/dto/request/GeneralPostUpdateRequestDto.java
@@ -1,11 +1,9 @@
 package com.example.capstone.dto.request;
 
-import com.example.capstone.entity.community.general.article.GeneralPost;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.sql.Blob;
 
 @Data
@@ -13,20 +11,7 @@ import java.sql.Blob;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GeneralPostUpdateRequestDto {
-    private Integer articleId;
     private String title;
     private Blob thumbnail;
     private String content;
-
-    /**
-     * GeneralPostUpdateRequestDto를 엔티티로 변환
-     */
-    public GeneralPost toEntity() {
-        return GeneralPost.builder()
-                .articleId(articleId)
-                .title(title)
-                .thumbnail(thumbnail)
-                .content(content)
-                .build();
-    }
 }
